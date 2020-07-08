81713ea8d3531e46403e010f7d28383f060597ed
username <kjw8208@nate.com>
2020-07-08 09:48:33 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log('im working im js im workl');
+/*string
+const what = "jinwon";
+*/
+/*boolean
+const wat = true;
+*/
+/*Number
+const wat = 66666;
+*/
+/*float
+const wat = 445.5;
+*/
+/* array data
+const monday  = "mon";
+const tue  = "mon";
+const wed  = "wed";
+const thu  = "thu";
+const fri  = "fri";
+
+const dayOfWeek = ['1','2','3','4','5'];
+dayOfWeek.push('6');
+*/
+/*Object*/
+// const name = 'jinwown'
+const jinwonInfo = {
+    name : "jinwon",
+    age : "28",
+    gender :"boy",
+    hensome : true,
+    favMovies : ["올드보이","헌츠맨","더헌트"],
+    favFood : [
+        {
+            name:"kimchi",
+            fatty:false
+        },
+        {
+            name:"buger",
+            fatty:true
+        }
+    ]
+};
+
